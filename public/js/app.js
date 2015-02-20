cde74409813acc38b0cddc234ee15577b10ce795
dcjohnston <djohnston2014@gmail.com>
2015-02-19 22:36:27 -0500

    enable express routing

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,33 @@
-define(['angularAMD'], function(angularAMD){
-    var myApp = angular.module('myApp', []);
+define(['angularAMD','models', 'localStorage', 'ui-router'], function(angularAMD){
+
+    // if my app requires a module, i have to make sure
+    // that the file containing it has been loaded
+    var myApp = angular.module('myApp', ['modelsModule', 'LocalStorageModule', 'ui.router']);
+
+    myApp.config(function(localStorageServiceProvider, $stateProvider, $urlRouterProvider, $locationProvider){
+        localStorageServiceProvider.setPrefix('dayToDay');
+        localStorageServiceProvider.setNotify(true, true);
+
+        $locationProvider.html5Mode({
+            enabled: true,
+            requireBase: false
+        });
+
+        $urlRouterProvider.when('/', '/root');
+        $urlRouterProvider.otherwise("/todos");
+
+        $stateProvider.state('root', {
+            url: "/todos",
+        })
+        .state('root.complete', {
+            url: '/finished'
+        })
+        .state('root.todo', {
+            url: '/incomplete'
+        });
+    });
 
     // when this module is required, we get var myApp as argument
     return angularAMD.bootstrap(myApp);
 });
+
--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,23 @@
 (function(){
     require.config({
         paths: {
-            'angular': '../bower_components/angular/angular',
-            'angularAMD': '../bower_components/angularAMD/angularAMD'
+            'angular': '/bower_components/angular/angular',
+            'angularAMD': '/bower_components/angularAMD/angularAMD',
+            'ngload': '/bower_components/angularAMD/ngload',
+            'localStorage': '/bower_components/angular-local-storage/dist/angular-local-storage',
+            'ngAnimate': '/bower_components/angular-animate/angular-animate',
+            'ui-router': '/bower_components/angular-ui-router/release/angular-ui-router',
+            'ngResource': '/bower_components/angular-resource/angular-resource'
         },
         shim: {
             'angularAMD': ['angular'],
+            'ngload': ['angularAMD'],
+            'localStorage': ['angular'],
+            'ngAnimate': ['angular'],
+            'ui-router':['angular'],
+            'ngResource': ['angular']
         },
+        // kick off the application by requiring app.js
         deps: ['app']
-
-
     });
-})()
+})();
